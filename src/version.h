ac2934b5448395e41f85bcc3ffb5fcb001f28719
regorxxx <regorxxx@protonmail.com>
2025-01-28 16:46:21 +0100

    cha: Component name is now displayed as "HTTP Control (fb2kc)" to further differentiate from the original version (but it shares the same dll name and preferences);

--- src/version.h
+++ src/version.h
@@ -3,16 +3,17 @@
 
 #define VERSION_MAJOR               0
 #define VERSION_MINOR               97
-#define VERSION_REVISION            28
+#define VERSION_REVISION            30
 #define VERSION_BUILD               0
+#define VERSION_ID_STR				"fb2kc"
 
 #define VER_PRODUCTNAME_STR         "foo_httpcontrol"
-#define VER_FILE_DESCRIPTION_STR    "HTTP Control"
+#define VER_FILE_DESCRIPTION_STR    "HTTP Control" " (" VERSION_ID_STR ")"
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
-#define VER_FILE_VERSION_STR        STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR(VERSION_REVISION) "." STR(VERSION_BUILD)
+#define VER_FILE_VERSION_STR        STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR(VERSION_REVISION) "." STR(VERSION_BUILD) "-" VERSION_ID_STR
 #define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
 #define VER_ORIGINAL_FILENAME_STR   VER_PRODUCTNAME_STR ".dll"
-#define VER_COPYRIGHT_STR           "(C) oblikoamorale"
+#define VER_COPYRIGHT_STR           "(C) oblikoamorale, (C) regorxxx"
 #define VER_SUPPORT_FORUM			"https://hydrogenaud.io/index.php/topic,62218.0.html"
 
 #ifdef _DEBUG
